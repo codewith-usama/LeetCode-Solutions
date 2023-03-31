b6d2f8ed341ac58d3a03ddd7ad63bc0356066814
Usama <epiicuree@gmail.com>
2023-04-01 01:38:17 +0500

    added question 242

---
+++ 242. Valid Anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length())
+            return false;
+
+        char[] s1 = s.toCharArray();
+        char[] s2 = t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        s = Arrays.toString(s1);
+        t = Arrays.toString(s2);
+        return s.equals(t);
+    }
+}
