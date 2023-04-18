1d36f869fc8cda6f65bb3b83ab6667c09d1b2dec
Usama <epiicuree@gmail.com>
2023-04-18 06:23:20 +0500

    added question 1768

---
+++ 1768. Merge Strings Alternately.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+        int i = 0, j = 0;
+        while (i < word1.length() || j < word2.length()) {
+            if (i < word1.length()) {
+                result.append(word1.charAt(i));
+                i++;
+            }
+            if (j < word2.length()) {
+                result.append(word2.charAt(j));
+                j++;
+            }
+        }
+        return result.toString();
+    }
+}
