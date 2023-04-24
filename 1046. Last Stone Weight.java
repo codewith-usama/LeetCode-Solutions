015e02aa4f264cb237ed6aab609da4bbc343ce66
Usama <epiicuree@gmail.com>
2023-04-24 11:14:16 +0500

    added question 1046

---
+++ 1046. Last Stone Weight.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> b - a);
+
+        for (int stone : stones) {
+            pq.offer(stone);
+        }
+
+        while (pq.size() > 1) {
+            int y = pq.poll();
+            int x = pq.poll();
+
+            if (x != y) {
+                pq.offer(y - x);
+            }
+        }
+
+        return pq.isEmpty() ? 0 : pq.poll();
+    }
+}
