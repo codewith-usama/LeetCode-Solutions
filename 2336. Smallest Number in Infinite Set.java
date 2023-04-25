004f69a701007fb2d11e3e02124da7d18c6bbb1d
Usama <epiicuree@gmail.com>
2023-04-25 07:40:13 +0500

    added question 2336

---
+++ 2336. Smallest Number in Infinite Set.java
@@ -0,0 +1,24 @@
+class SmallestInfiniteSet {
+    public int popSmallest() {
+        if (added.isEmpty())
+            return curr++;
+        final int min = added.first();
+        added.remove(min);
+        return min;
+    }
+
+    public void addBack(int num) {
+        if (num < curr)
+            added.add(num);
+    }
+
+    private int curr = 1;
+    private TreeSet<Integer> added = new TreeSet<>();
+}
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet obj = new SmallestInfiniteSet();
+ * int param_1 = obj.popSmallest();
+ * obj.addBack(num);
+ */
