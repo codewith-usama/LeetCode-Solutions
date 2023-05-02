aa67713e414c8123b15da44f071943bd0f825f26
Usama <epiicuree@gmail.com>
2023-05-02 19:58:10 +0500

    added question 1822

---
+++ 1822. Sign of the Product of an Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            count += (nums[i] < 0) ? 1 : 0;
+            if(nums[i] == 0) {
+                return 0;
+            }
+        }
+        return (count%2 == 0) ? 1 : -1;
+    }
+};
